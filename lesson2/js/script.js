489cfc52719b919cf65edc897bbd777c44028993
Dagwan <dagwanpan@gmail.com>
2022-05-13 19:55:39 +0100

    adjustment

---
+++ lesson2/js/script.js
@@ -0,0 +1,8 @@
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
+
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.getElementById("year").textContent = Year;
+
+
