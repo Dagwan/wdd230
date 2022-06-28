e93f41fb2b1c431cd4ccb7c77a1f6fab3f0221a0
Dagwan <dagwanpan@gmail.com>
2022-06-28 11:48:09 +0100

    contact page

---
+++ lesson11/js/script.js
@@ -0,0 +1,36 @@
+/* Get Current Date for Header */
+const currentDateSpan = document.querySelector("#currentDate");
+
+const now = new Date();
+currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
+  dateStyle: "full",
+}).format(now);
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+  }
+  
+  const x = document.getElementById('hamburgerBtn')
+  x.onclick = toggleMenu;
+
+/* Date for the footer*/
+let newDate = new Date();
+
+document.getElementById("currentYear").textContent = newDate.getFullYear();
+
+let lastUpdated = document.lastModified;
+
+document.getElementById("updatedDate").textContent = lastUpdated;
+
+/* Banner on Mondays & Tuesdays */
+
+if(now.getDay() === 2 || now.getDay() === 1) {
+  document.getElementById("banner").style.display = "block";
+}
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+  banner.style.display = "none";
+});
